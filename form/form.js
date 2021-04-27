c455b0b7756301838fe20e0ad19d2affc211e205
navanithpk <69448465+navanithpk@users.noreply.github.com>
2021-04-27 17:22:59 +0530

    Introduction of "Static" folder

--- form/form.js
+++
@@ -1,7 +0,0 @@
-function EnableDisableTextBox(chkOthers) {
-    var OtherField = document.getElementById('other');
-    OtherField.disabled = chkOthers.checked ? false : true;
-    if (!OtherField.disabled) {
-        OtherField.focus();
-    }
-}
